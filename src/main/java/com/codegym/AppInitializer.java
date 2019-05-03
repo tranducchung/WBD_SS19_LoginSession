a1294ab3ce430b9b7e1e1650d823fb8e69852462
tranchung <tranducchung1st@gmail.com>
2019-05-03 10:06:44 +0700

    Finish app Save-User

---
+++ src/main/java/com/codegym/AppInitializer.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+import com.codegym.AppConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
